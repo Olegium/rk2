ccc69070dad6955fb9d2276768a68f4732584b12
Oleg Shatrov Alekseevich <shatroffoleg@gmail.com>
2025-06-06 18:10:40 +0300

    Update ConcreteCommandA.h

--- .h/ConcreteCommandA.h
+++ .h/ConcreteCommandA.h
@@ -1,1 +1,12 @@
+#include "ICommand.h"
+#include "IReciever.h"
 
+class ConcreteCommandA : public ICommand
+{
+public:
+    ConcreteCommandA(IReciever * reciever);
+    virtual void execute() const override;
+
+private:
+    IReciever *m_reciever;
+};
